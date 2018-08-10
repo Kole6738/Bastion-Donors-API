6adc4f73295b612d1991643f4c3df4eade7b3729
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-08-10 13:06:25 +0000

    app: add main server file

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors')
+const logger = require('morgan');
+require('dotenv').config();
+
+const patreonRouter = require('./routes/patreon');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/patreon', patreonRouter);
+
+// Catch 404
+app.use((req, res, next) => {
+  res.json({
+    error: '404',
+    message: 'Not Found'
+  });
+});
+
+module.exports = app;
