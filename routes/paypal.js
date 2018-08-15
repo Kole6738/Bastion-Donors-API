9498d9bf710d6a6d7b43925ebeba2066ef1ec523
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-08-15 10:09:26 +0000

    routes: /paypal will include Discord user tags

--- routes/paypal.js
+++ routes/paypal.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const Discord = require('discord.js');
 const request = require('request-promise-native');
 const router = express.Router();
+const client = new Discord.Client();
 const donorsData = require('../data/donorsData.json');
 
-router.get('/', (req, res, next) => {
-  res.json(donorsData);
+router.get('/', async (req, res, next) => {
+  try {
+    let options = {
+      json: true,
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
+      }
+    };
+
+    let donors = [];
+    for (let donor of donorsData) {
+      let url = `https://discordapp.com/api/users/${donor.discord_id}`;
+      let response = await request(url, options);
+
+      donor.usertag = `${response.username}#${response.discriminator}`
+    }
+
+    res.json(donorsData);
+  }
+  catch (e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
